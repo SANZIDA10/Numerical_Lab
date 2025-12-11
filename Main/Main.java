14e94da5482e98b2f893d920d97cc5ad0b5c74db
SANZIDA10 <sanzidajerin28@gmail.com>
2025-12-11 19:56:50 +0600

    Test Commit

---
+++ Main/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+static int sum(int c,int d){
+return c+d;
+}
+    public static void main(String... args){
+        int a=50;
+        int b=30;
+    System.out.println("sum(a,b)");
+    }
+}
