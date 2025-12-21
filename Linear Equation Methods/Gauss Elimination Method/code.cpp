294286734e4f85b57f240a2e6e609df1e57282d1
SANZIDA10 <sanzidajerin28@gmail.com>
2025-12-21 09:42:35 +0600

    Update code.cpp

--- Linear Equation Methods/Gauss Elimination Method/code.cpp
+++ Linear Equation Methods/Gauss Elimination Method/code.cpp
@@ -1,35 +1,30 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
 using namespace std;
 
 int main() {
-    ifstream fin("input.txt");  
-    ofstream fout("output.txt"); 
-
-    if(!fin) {
-        cout << "Error: input.txt not found!" << endl;
-        return 0;
-    }
+    ifstream fin("input.txt");
+    ofstream fout("output.txt");
 
     int n;
-    fin >> n;  
+    fin >> n;
 
-    vector<vector<float>> a(n, vector<float>(n + 1));
+    int a[10][11], x[10];
+
     for (int i = 0; i < n; i++)
         for (int j = 0; j <= n; j++)
             fin >> a[i][j];
 
-    
     for (int i = 0; i < n - 1; i++) {
         for (int k = i + 1; k < n; k++) {
-            float factor = a[k][i] / a[i][i];
-            for (int j = i; j <= n; j++) {
-                a[k][j] -= factor * a[i][j];
-            }
+            int f = a[k][i] / a[i][i];
+            for (int j = i; j <= n; j++)
+                a[k][j] -= f * a[i][j];
         }
     }
 
-    
-    vector<float> x(n, 1);
     for (int i = n - 1; i >= 0; i--) {
         x[i] = a[i][n];
         for (int j = i + 1; j < n; j++)
@@ -37,11 +32,15 @@ int main() {
         x[i] /= a[i][i];
     }
 
-    
-    fout << fixed << setprecision(2);
-    fout << "Solution:\n";
     for (int i = 0; i < n; i++)
-        fout << "x" << i + 1 << " = " << x[i] << "\n";
+        fout << "x" << i + 1 << " = " << x[i] << endl;
+
+    fout << "\nFinal Upper Triangular Matrix:\n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= n; j++)
+            fout << setw(5) << a[i][j] << " ";
+        fout << endl;
+    }
 
     fin.close();
     fout.close();
